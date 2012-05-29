14d749bf36ee09616fc863481ef750979def0327
Brad Kahl <bkahl1984@gmail.com>
2012-05-29 10:36:20 -0400

    added procfile and updated port in app file

--- app.js
+++ app.js
@@ -6,7 +6,8 @@
 var express = require('express'),
     ArticleProvider = require('./articleprovider-memory').ArticleProvider,
     articleProvider = new ArticleProvider(),
-    app = module.exports = express.createServer();
+    app = module.exports = express.createServer(),
+    port = process.env.PORT || 3000;
 
 // Configuration
 
@@ -77,6 +78,6 @@ app.post('/blog/new', function(req, res){
 //        });
 // });
 
-app.listen(3000, function(){
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
